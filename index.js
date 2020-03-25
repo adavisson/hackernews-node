8d9584c63d180c9265ad0c26ea184b398d510723
Andrew Davisson <akdavisson4@gmail.com>
2020-03-25 11:35:54 -0600

    implemented show link and delete link

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ let links = [{
   description: 'FullStack tutorial for GraphQL'
 }]
 
+let idCount = links.length;
+
 const resolvers = {
   Query: {
     info: () => `This is the API for a Hackernews Clone`,
     feed: () => links,
+    link: (parent, args) => {
+      const link = links.find(el => el.id == args.id);
+      return link
+    } 
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link);
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      const link = links.find(el => el.id == args.id);
+      const index = links.indexOf(link);
+      links.splice(index, 1);
+      return link;
+    }
   }
 }
 
