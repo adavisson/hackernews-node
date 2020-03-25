5321ec36eac3650137f72d25cfb8d423979e0682
Andrew Davisson <akdavisson4@gmail.com>
2020-03-25 12:55:34 -0600

    update schema to get rid of mutations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./src/generated/prisma-client');
 
 const resolvers = {
   Query: {
@@ -20,5 +21,6 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
